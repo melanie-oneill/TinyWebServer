4c0b9368ab2be8ad3896fabf07502d615c7f89c8
melanie-oneill <moneill6@student.cscc.edu>
2019-11-12 21:20:13 -0500

    Part 2 HTTPRequest class done. Still needs JUnit and JavaDoc

--- src/edu/cscc/HTTPRequest.java
+++ src/edu/cscc/HTTPRequest.java
@@ -1,12 +1,18 @@
 package edu.cscc;
 
+
+import java.io.File;
+
 public class HTTPRequest {
     private String request;         // request string
     private String path;            // path to file
     private boolean validRequest;   // is request valid?
 
+
     public HTTPRequest(String r) {
-        // TODO Constructor
+        this.request = request;
+        this.path = path;
+        this.validRequest = validRequest;
     }
 
     public boolean isValidRequest() {
@@ -17,8 +23,32 @@ public class HTTPRequest {
         return (path);
     }
 
-    // TODO uncomment
-    // private boolean parse(String r) {
-        //  TODO code here
-    // }
+    private boolean parse(String r) {
+       String parsedString = getPath();
+       String delimiters = ("[\t\n?]") ;
+       String[] stringParts = parsedString.split(delimiters);
+       File file = new File(stringParts[1]);
+       if(stringParts.length >= 2 && stringParts[0] == "GET" && file.exists()){
+           System.out.println("Received GET and file exists");
+           validRequest = true;
+           return validRequest;
+       } else if (!file.exists()){
+           System.out.println("file does not exist");
+           validRequest = false;
+           return validRequest;
+       } else if(stringParts.length < 2){
+           System.out.println("array has less than two values.");
+           validRequest = false;
+           return validRequest;
+       } else if(stringParts[0] != "GET"){
+           System.out.println("Request is not a get.");
+           validRequest = false;
+           return validRequest;
+       } else {
+           System.out.println("something went wrong in parse.");
+           validRequest = false;
+           return validRequest;
+       }
+
+    }
 }
